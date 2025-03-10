6679213f6c68120764d63d4de502ad4f6021e238
Jason Ericson <jasonlericson@gmail.com>
2025-03-09 23:09:40 -0500

    Part 3 exercises 3.1-3.4 applied

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log('Running initialized package');
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Phonebook backend</h1>');
+});
+
+app.get('/info', (request, response) => {
+  let numberOfPersons = persons.length;
+  let personsText = numberOfPersons === 1 ? 'person' : 'people';
+
+  response.send(
+    `<div>Phonebook has info for ${numberOfPersons} ${personsText}<br /><br />${new Date().toString()}</div>`
+  );
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = `'${request.params.id}' is not a valid person id`;
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
