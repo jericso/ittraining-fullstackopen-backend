7d06dd096fd94ae9d7a779acb5f7ab936feed7ca
Jason Ericson <jasonlericson@gmail.com>
2025-03-10 14:22:21 -0500

    Part 3a - Middleware

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
 app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -73,6 +82,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
