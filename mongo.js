8d9d4ddc0e44b6fa4a5fc3c7ad3b3de648996aa3
Jason Ericson <jasonlericson@gmail.com>
2025-03-15 14:42:16 -0500

    Part 3 exercise 3.12 applied

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const nameAdd = process.argv[3];
+const numberAdd = process.argv[4];
+
+const url = `mongodb+srv://jericso:${password}@cluster0.rr0l9.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (nameAdd || numberAdd) {
+  const person = new Person({
+    name: nameAdd,
+    number: numberAdd,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
