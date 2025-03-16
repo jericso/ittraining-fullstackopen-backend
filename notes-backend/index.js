e94782c9efbda8a7ef4fe062a48f84a4034feabd
Jason Ericson <jasonlericson@gmail.com>
2025-03-16 13:16:39 -0500

    Part 3c - Moving error handling into middleware

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -49,7 +49,7 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -58,10 +58,7 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'malformatted id' });
-    });
+    .catch((error) => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -93,6 +90,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
