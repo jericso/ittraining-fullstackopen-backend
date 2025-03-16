5575b88d21c5ceb11222fa000660cfa089284bad
Jason Ericson <jasonlericson@gmail.com>
2025-03-16 14:57:05 -0500

    Part 3 exercises 3.15-3.18 applied

--- index.js
+++ index.js
@@ -27,64 +27,47 @@ app.use(
 );
 app.use(express.static('dist'));
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook backend</h1>');
 });
 
-app.get('/info', (request, response) => {
-  Person.find({}).then((persons) => {
-    let numberOfPersons = persons.length;
-    let personsText = numberOfPersons === 1 ? 'person' : 'people';
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      let numberOfPersons = persons.length;
+      let personsText = numberOfPersons === 1 ? 'person' : 'people';
 
-    response.send(
-      `<div>Phonebook has info for ${numberOfPersons} ${personsText}<br /><br />${new Date().toString()}</div>`
-    );
-  });
+      response.send(
+        `<div>Phonebook has info for ${numberOfPersons} ${personsText}<br /><br />${new Date().toString()}</div>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id;
 
-  Person.findById(id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.statusMessage = `'${id}' is not a valid person id`;
-      response.status(404).end();
-    }
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.statusMessage = `'${id}' is not a valid person id`;
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -98,17 +81,55 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
 
-  response.status(204).end();
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end();
+      }
+
+      person.name = name;
+      person.number = number;
+
+      return person
+        .save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+    })
+    .catch((error) => next(error));
 });
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
