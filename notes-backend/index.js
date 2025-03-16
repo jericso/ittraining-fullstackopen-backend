4b63e1d6f8bb63bc57a702af6f03d188acbeb690
Jason Ericson <jasonlericson@gmail.com>
2025-03-16 13:41:19 -0500

    Part 3c - Other operations

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -78,11 +78,31 @@ app.post('/api/notes', (request, response) => {
   });
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body;
 
-  response.status(204).end();
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
