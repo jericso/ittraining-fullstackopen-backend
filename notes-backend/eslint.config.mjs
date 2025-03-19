4bebfa73fac91a3529b8919b7d662e843a198d70
Jason Ericson <jasonlericson@gmail.com>
2025-03-18 21:09:46 -0500

    Part 3d - Adding More Style Rules

--- notes-backend/eslint.config.mjs
+++ notes-backend/eslint.config.mjs
@@ -18,7 +18,12 @@ export default [
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
+      '@stylistic/js/semi': ['error', 'always'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
 ];
